b0bf9f131d3f31a693e53facb7eea061f3737206
Michel Vidailhet <michel.vidailhet@gmail.com>
2016-08-29 15:48:42 +0200

    bootstrap app even if json file has not been found

--- package/src/ConfigurationProvider.ts
+++ package/src/ConfigurationProvider.ts
@@ -36,9 +36,10 @@ export function loadConfigurationJSON(callback) {
     xobj.overrideMimeType("application/json");
     xobj.open('GET', 'configuration.json', true); // Replace 'my_data' with the path to your file
     xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == 200) {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            (<any>window).configuration = JSON.parse(xobj.responseText);
+        if (xobj.readyState == 4) {
+            if (xobj.status == 200) {
+                (<any>window).configuration = JSON.parse(xobj.responseText);
+            }
             callback();
         }
     };
